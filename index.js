0ac312a5373b23e378487f0d9e5291072bac38c4
Dylan Trepos <dylan.trepos@gmail.com>
2022-03-02 14:05:38 +0100

    Add sameSie

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(session({
     name: 'connect.sid',
     cookie: {
         expires: 1000 * 60 * 60 * 24,
-        secure: true,
+        secure: false,
         httpOnly: true,
         sameSite: 'none'
     }
